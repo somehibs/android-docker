62866ae3a0a5ad5c1c1e6081f985d48294831bc7
greed <greed@greed>
2018-08-01 21:23:19 +0100

    script works, dockerfile works

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@
 ANDROID_SDK_TOOLS=24.4.1
 mkdir build
 cd build/
-wget --quiet --output-document=android-sdk.tgz https://dl.google.com/android/android-sdk_r${ANDROID_SDK_TOOLS}-linux.tgz
+echo "Fetching Android SDK"
+wget --continue --quiet --output-document=android-sdk.tgz https://dl.google.com/android/android-sdk_r${ANDROID_SDK_TOOLS}-linux.tgz
+echo "Building Android docker image"
+cp ../android Dockerfile
 docker build -t android .
